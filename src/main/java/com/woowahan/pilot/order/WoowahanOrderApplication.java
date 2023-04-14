a548095db838c9ef7ae2d8bd77bddb99db957a83
조현창 <gusckd57@chungbuk.ac.kr>
2023-04-15 07:05:37 +0900

    Initial commit

---
+++ src/main/java/com/woowahan/pilot/order/WoowahanOrderApplication.java
@@ -0,0 +1,13 @@
+package com.woowahan.pilot.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowahanOrderApplication.class, args);
+	}
+
+}
