2ed73ed5da3bc9e5d4fa1027c1382b56201b5367
vcho1958 <gusckd57@gmail.com>
2023-05-07 18:08:19 +0900

    feat(exception): add CustomException

--- src/main/java/com/woowahan/pilot/user/common/exception/CustomException.java
+++ src/main/java/com/woowahan/pilot/user/common/exception/CustomException.java
@@ -1,14 +1,13 @@
 package com.woowahan.pilot.user.common.exception;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Getter
+@AllArgsConstructor
 public enum CustomException {
   DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "중복된 이메일입니다."),
   DUPLICATED_PHONE(HttpStatus.BAD_REQUEST, "중복된 휴대폰 번호입니다."),
   DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다.");
-
-  CustomException(HttpStatus status, String message) {
-  }
+  public final HttpStatus status;
+  public final String message;
 }
